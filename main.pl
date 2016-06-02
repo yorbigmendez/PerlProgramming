96746094f241772b640a79a98cee20bd287996de
Yorbi Mendez <yorbigmendez@gmail.com>
2016-06-02 08:24:29 -0600

    Last changes

--- main.pl
+++ main.pl
@@ -2,12 +2,78 @@
 
 # inicializar lista de contactos
 @directorio = ();
-
+use feature qw(switch);
 require "ContactoEmpresarial.pm";
 require "ContactoComun.pm";
 
 sub mainPrincipal{
 
+	print "\n\n\n****Bienvenido al sistema Agenda Contactos Perl****";
+    $opcion = 0;
+    do{
+    print "\n Que desea realizar: ";
+    print "\n\t1) Agregar Contacto.";
+    print "\n\t2) Actualizar Contacto.";
+    print "\n\t3) Eliminar Contacto.";
+    print "\n\t4) Ver mis Contactos.";
+    print "\n\t5) Salir.";
+    print "\n\n-----------------------\nAccion: ";
+	$opcion = <STDIN>;
+    
+        if ($opcion == 1) {
+            print "\nOpciones de Contacto: \n";
+            print "\t1)Contacto Comun.";
+            print "\n\t1)Contacto Empresarial.";
+            print "\nQue tipo de contacto desea agregar: ";
+            $val = <STDIN>;
+            given($val){
+                when(1){
+                    print "Ingrese el nombre del Contacto: ";
+                    $nombre = <STDIN>;
+                     print "Ingrese numero de telefono 1: ";
+                    $telefono1 = <STDIN>;
+                     print "Ingrese numero de telefono 2: ";
+                    $telefono2 = <STDIN>;
+                    agregarContactoComun($nombre,$telefono1,$telefono2);
+                    print"\n\n\n\nAGREGADO CON EXITO!!!\n\n\n\n";
+                    
+                }
+                when(2){
+                    print "Ingrese el nombre del Contacto: ";
+                    $nombre = <STDIN>;
+                     print "Ingrese numero de telefono: ";
+                    $telefono1 = <STDIN>;
+                    print "Ingrese correo dominio: ";
+                    $dominio = <STDIN>;
+                     print "Ingrese correo electronico: ";
+                    $email = <STDIN>;
+                    print "Ingrese ubicacion de contacto: ";
+                    $ubicacion = <STDIN>;
+                    agregarContactoEmpresarial($nombre,$telefono1,$telefono2,$dominio,$email,$ubicacion);
+                    print"\n\n\n\nAGREGADO CON EXITO!!!\n\n\n\n";
+                }
+                when(3){
+                    $nombre = <STDIN>;
+                    
+                }
+                default {
+                print "Holy molly guacamole! error";
+                }
+            }
+        }elsif ($opcion cmp 2){
+            print "chango";
+        }elsif ($opcion cmp 3){
+            print "Ingrese el nombre del contacto que desea eliminar: ";
+        }elsif ($opcion cmp 4){
+            print "chango";
+        }elsif ($opcion cmp 5){
+            print "chango";
+        }else{
+            print "Error!. Opcion no valida";
+        }   
+    
+    }while($opcion != 5);
+    
 	agregarContactoEmpresarial("Carla Vargas",88885798,"www.tec.com","carlaVargas.ac.cr","San Carlos"); 
 	agregarContactoEmpresarial("Karol Rosales",950620,"www.utn.com","KarolRosales.ac.cr","Zarcero"); 
 	agregarContactoEmpresarial("Mario Gonzalez",7820093,"www.sancarlos.com","MarioG.ac.cr","Fortuna");
@@ -15,9 +81,8 @@ sub mainPrincipal{
 	agregarContactoComun("Yerlin Gomez",88517896,890204); 
 	agregarContactoComun("Hector Cruz",88638947,885014750); 
 	agregarContactoComun("Oscar Viquez",8836520,9605200);
-
+	
 }
-
 
 sub imprimir{
 	my $i;
@@ -43,14 +108,7 @@ sub imprimir{
 
 
 #agregar contactos comunes a la lista de directorio
-sub agregarContactoComun{
-	my $nombre = shift;
-	my $telefono = shift;
-	my $telefono2 = shift;
-	my $ObjContacto = new ContactoComun($nombre, $telefono,$telefono2);
-	push(@directorio, $ObjContacto);
-	#print "\n\n Contacto Agregado, direcctorio Actualizado: ", @directorio;
-}
+
 sub eliminarPersonaNombre{
 	my $i;
 	my $index = 0;
@@ -93,7 +151,14 @@ sub listMyContacts{
     return $index;
 
 }
+#uso de actualizar info
+sub actualizarInfo{
+    print "chango";
+}
 
+sub agregarContactoComun{
+	print "chango";
+}
 #agregar a la lista
 sub agregarContactoEmpresarial{
 	my $nombre = shift;
@@ -106,19 +171,7 @@ sub agregarContactoEmpresarial{
 	#print "\n\nContacto Agregado, direcctorio Actualizado: ", @directorio;
 }
 
-#agregar elemento al incio de la lista
-sub agregarElementoInicio{
-	my $nombre = shift;
-	my $telefono = shift;
-	my $direccionWeb = shift;
-	my $correoElectronico = shift;
-	my $ubicacion = shift;
-	my $ObjContacto = new ContactoEmpresarial($nombre, $telefono,$direccionWeb, $correoElectronico,$ubicacion);
-	unshift(@directorio,$ObjContacto);
-	print "\n\nContacto Agregado al inicio, direcctorio Actualizado: ", @directorio;	
-}
-
-
 &mainPrincipal(); #llamar el metodo principal de la clase
 
-listMyContacts();
+#listMyContacts();
+actualizarInfo();
